59fdd8404e9f396494e9d141630ea3702f8f57e3
Aran Abrahamlingam <aran0713@hotmail.com>
2023-04-29 16:28:16 -0400

    First take

---
+++ EventHiveApp/mapData.ts
@@ -0,0 +1,47 @@
+export const Markers = [
+    {
+        coordinate: {
+            latitude: 42.3099844,
+            longitude: -83.0622765,
+        },
+        title: "House Party",
+        smallDescription: "To kick off the new year",
+        privacy: "Private",
+        date:"Fri April 5th",
+        time: "8:00pm-2:00am"
+    },
+    {
+        coordinate: {
+            latitude: 42.305650,
+            longitude: -83.060390,
+        },
+        title: "Liquid Library",
+        smallDescription: "10$ Burgers",
+        privacy: "Public",
+        date:"Fri April 6th",
+        time: "4:00pm-2:00am"
+    },
+    {
+        coordinate: {
+            latitude: 42.314810,
+            longitude: -83.037550,
+        },
+        title: "Wild Child",
+        smallDescription: "3$ Drinks",
+        privacy: "Public",
+        date:"Fri April 5th",
+        time: "10:00pm-1:00am"
+    },
+    {
+        coordinate: {
+            latitude: 42.305385,
+            longitude: -83.061534,
+        },
+        title: "Local Bar",
+        smallDescription: "More Drinks",
+        privacy: "Public",
+        date:"Fri April 8th",
+        time: "7:00pm-10:00pm"
+    },
+
+]
